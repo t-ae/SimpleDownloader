afec5828b04554dc984b6b8a9d8a574e4be28ec6
araki <araki@qoncept.co.jp>
2016-11-08 15:04:47 +0900

    Regenerate xcodeproj, add target

---
+++ SimpleDownloader-OSX/SimpleDownloader-OSX.h
@@ -0,0 +1,19 @@
+//
+//  SimpleDownloader-OSX.h
+//  SimpleDownloader-OSX
+//
+//  Created by Araki Takehiro on 2016/11/08.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SimpleDownloader-OSX.
+FOUNDATION_EXPORT double SimpleDownloader_OSXVersionNumber;
+
+//! Project version string for SimpleDownloader-OSX.
+FOUNDATION_EXPORT const unsigned char SimpleDownloader_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleDownloader_OSX/PublicHeader.h>
+
+
