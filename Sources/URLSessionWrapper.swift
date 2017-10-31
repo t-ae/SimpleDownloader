b6c2c4af13420b7c0f556a2bf99f0956078ec51c
araki <araki@qoncept.co.jp>
2017-10-31 09:58:25 +0900

    preconditions

--- Sources/URLSessionWrapper.swift
+++ Sources/URLSessionWrapper.swift
@@ -16,18 +16,22 @@ internal protocol URLSessionWrapper : class {
 
 extension URLSessionWrapper {
     public func onProgress(_ handler: @escaping (Double)->Void) {
+        precondition(progressHandler == nil, "`progressHandler` is already set.")
         progressHandler = handler
     }
     
     public func onComplete(_ handler: @escaping (Self.ResultType)->Void) {
+        precondition(completionHandler == nil, "`completionHandler` is already set.")
         completionHandler = handler
     }
     
     public func onCompleteWithError(_ handler: @escaping (Error)->Void) {
+        precondition(errorHandler == nil, "`errorHandler` is already set.")
         errorHandler = handler
     }
     
     public func onCancel(_ handler: @escaping ()->Void) {
+        precondition(cancelHandler == nil, "`cancelHandler` is already set.")
         cancelHandler = handler
     }
     
